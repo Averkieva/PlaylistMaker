44d522faa17144bee29c1b0066ce74f0ae2a9c5b
Ksenia <averkieva10012017@yandex.ru>
2023-04-09 22:30:38 +0300

    Commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sBut = findViewById<Button>(R.id.search)
+        val mBut = findViewById<Button>(R.id.media)
+        val setBut = findViewById<Button>(R.id.settings)
+        sBut.setOnClickListener{
+            //Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск", Toast.LENGTH_SHORT).show()
+            val searchIntent = Intent(this@MainActivity, SearchingActivity::class.java)
+            startActivity(searchIntent)
+        }
+       // val mediaClickListener: View.OnClickListener = object : View.OnClickListener {
+           // override fun onClick(v: View?) {
+              //  Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека", Toast.LENGTH_SHORT).show()
+            //}
+       // }
+        mBut.setOnClickListener {
+            val mediaIntent = Intent(this@MainActivity, MediatekaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+        setBut.setOnClickListener {
+            //Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки", Toast.LENGTH_SHORT).show()
+            val settingIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(settingIntent)
+        }
     }
 }
